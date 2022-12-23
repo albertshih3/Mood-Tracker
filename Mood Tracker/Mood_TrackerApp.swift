34e76c1a20a60e2280fcc0be053df2fedb76cd31
Albert Shih <albertshih3@gmail.com>
2022-12-23 12:00:58 -0800

    Initial Commit

---
+++ Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by Kaelin Shih on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
